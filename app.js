93b549b2b9b3bba701ce16cf718a7b76dff0f373
mailtohuy <mailtohuy@gmail.com>
2017-11-16 22:35:23 -0500

    Search now looks for both available and checked out devices. inventory.xlsx expected in same directory as app.js.

--- app.js
+++ app.js
@@ -8,31 +8,20 @@ inquirer.prompt([
     type: 'input',
     name: 'path',
     message: 'Path to inventory file:',
-    default: './data/inventory.xlsx'
-  },
-  {
-    type: 'list',
-    name: 'command',
-    message: 'What do you like to do?',
-    choices: [
-      { name: 'Search', value: 'search' }
-    ],
-    default: 'search'
+    when(session) {
+      session.path = './inventory.xlsx';
+      return !fs.existsSync(session.path);
+    }
   },
   {
     type: 'input',
     name: 'terms',
-    message: 'Enter search terms:',
-    when(session) {
-      return (session.command === 'search')
-    }
+    message: 'Search for:'
   },
 ]).then(session => {
-  console.log(session);
   let libman = init(session.path);
   console.log(libman.search(session.terms));
 });
-
 
 function init(inventory_file) {
   const isCharger = /.*charger.*/i;
@@ -44,54 +33,52 @@ function init(inventory_file) {
   const data = fs.readFileSync(inventory_file);
   var workbook = xlsx.read(new Uint8Array(data), { type: "array" });
   
-  const statusSheet = xlsx.utils.sheet_to_json(workbook.Sheets['Status']);
+  const statusSheet = xlsx.utils.sheet_to_json(workbook.Sheets['Status Log']);
   const userSheet = xlsx.utils.sheet_to_json(workbook.Sheets['Users']);
-  const deviceSheet = xlsx.utils.sheet_to_json(workbook.Sheets['Devices']);
+  const deviceSheet = xlsx.utils.sheet_to_json(workbook.Sheets['Devices-Phones']);
   
   var userList =
     _.chain(userSheet)
-      .map(user => _.pick(user, 'UserId', 'Name', 'Email'))
-      .groupBy('UserId')
+      .map(user => _.pick(user, 'User Id', 'First Name', 'Email'))
+      .groupBy('User Id')
       .value();
   
   var deviceList =
     _.chain(deviceSheet)
-      .map(device => _.chain(device).pick('DeviceId', 'Manufacturer', 'Model', 'OSVersion').defaults({ 'OSVersion': '-' }).value())
-      .groupBy('DeviceId')
+      .map(device => _.chain(device).pick('Id', 'Manufacturer', 'Model', 'OS Version').defaults({ 'OS Version': '-' }).value())
+      .groupBy('Id')
       .value();
   
   var checkedOutList =
     _.chain(statusSheet)
       /* join with userList to add user info. Note: this mutates statusSheet */
-      .map(checkedout => _.defaults(checkedout, _.pick(userList[checkedout['UserId']][0], 'Name', 'Email')))
+      .map(checkedout => _.defaults(checkedout, _.pick(userList[checkedout['User Id']][0], 'First Name', 'Email')))
       /* join with deviceList to add device info. Note: this mutates statusSheet */
-      .map(checkedout => _.defaults(checkedout, _.pick(deviceList[checkedout['DeviceId']][0], 'Manufacturer', 'Model', 'OSVersion')))
+      .map(checkedout => _.defaults(checkedout, _.pick(deviceList[checkedout['Device Id']][0], 'Manufacturer', 'Model', 'OS Version')))
       .value();
   
-  var checkedOutDeviceIds = _.pluck(checkedOutList, 'DeviceId');
-  
-  function listavailableDevices() {
-    return _.chain(deviceSheet)
-    .filter(device => !isCharger.test(device['Model'])
-      && !isCable.test(device['Model'])
-      && !isHotSpot.test(device['Model'])
-      && !isSimCard.test(device['Model'])
-      && _.indexOf(checkedOutDeviceIds, device['Id']) < 0)
+  var checkedOutDeviceIds = _.pluck(checkedOutList, 'Device Id');
+
+  var availableDeviceList =  _.chain(deviceSheet)
+    .filter(device =>  _.indexOf(checkedOutDeviceIds, device['Id']) < 0)
     .map(device => _.chain(device).defaults({ 'OS Version': '-' }).pick('Id', 'Manufacturer', 'Model', 'OS Version').value())
-    .groupBy('OS Version')
+    // .groupBy('OS Version')
     .value();
-  }
-  
-  function isMatchedAnyValue (obj, searchTerm) {
-      return _.any(_.keys(obj).map(key => re(searchTerm).test(obj[key])));
-  }
-  
+    
+
   function search(searchPhrase, objectList) {
-    return searchPhrase.split(' ').reduce( (list, searchTerm) => list.filter (obj => isMatchedAnyValue(obj, searchTerm)) , objectList );
+
+    function isMatchedAnyValue (obj, searchTerm) {
+      return _.any(_.keys(obj).map(key => re(searchTerm).test(obj[key])));
+    }
+
+    return searchPhrase.split(' ')
+    .map(term => term.replace('_',' '))
+    .reduce( (list, searchTerm) => list.filter (obj => isMatchedAnyValue(obj, searchTerm)) , objectList );
   }
 
   return {
-    search: terms=>search(terms,checkedOutList)
+    search: terms => search(terms,availableDeviceList).concat(search(terms,checkedOutList))
   }; 
 
 }
